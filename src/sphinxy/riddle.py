94eddb3800ef0688de0bf0fdc781de83513f8db3
Alessio Gorla <alessio.gorla@gmail.com>
2023-05-28 15:01:44 +0200

    Added docstrings

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -8,7 +8,20 @@ class Riddle:
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """ Function that checks if a given answer is right or not.
+
+        Args:
+            answer (str): _description_
+
+        Returns:
+            bool: _description_
+        """
         return answer.lower() == self.answer.lower()
 
     def get_hint(self) -> Iterator[str]:
+        """Function that gives a hint to the user on demand.
+
+        Yields:
+            Iterator[str]: _description_
+        """
         yield from iter(self.answer)
--- src/sphinxy/sphinx.py
+++ src/sphinxy/sphinx.py
@@ -9,6 +9,11 @@ class IncorrectAnswer(Exception):
 
 class Sphinx:
     def __init__(self, name: str) -> None:
+        """ Init the class Sphinx.
+
+        Args:
+            name (str): _description_
+        """
         self._name = name
         self._riddle = Riddle(
             question=(
@@ -19,6 +24,11 @@ class Sphinx:
         )
 
     def introduce(self) -> str:
+        """Introduction function that give details about the features of the website.
+
+        Returns:
+            str: _description_
+        """
         return (
             f"Greetings, mortals. I am {self._name}. I have guarded the city of Thebes"
             "for centuries and posed riddles to those who dared to approach me."
@@ -29,6 +39,14 @@ class Sphinx:
         return "I have updated my riddle. Are you ready to solve it?"
 
     def pose_riddle(self, include_hint: bool = False) -> tuple[str, str | None]:
+        """Function that manage the creation of a riddle.
+
+        Args:
+            include_hint (bool, optional): _description_. Defaults to False.
+
+        Returns:
+            tuple[str, str | None]: _description_
+        """
         hint = (
             f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
             if include_hint
@@ -37,6 +55,19 @@ class Sphinx:
         return (self._riddle.question, hint)
 
     def check_riddle_answer(self, answer: str, return_hint: bool = False) -> str:
+        """Evaluates the given answer to the riddle.
+
+        Args:
+            answer (str): _description_
+            return_hint (bool, optional): _description_. Defaults to False.
+
+        Raises:
+            IncorrectAnswer: _description_
+
+        Returns:
+            str: _description_
+        """
+ 
         if self._riddle.check_answer(answer):
             return "Your answer was correct. You may pass."
         elif return_hint:
